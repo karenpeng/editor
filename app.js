737395ebd712240bfd595d88a3da71bb8ea4ef3f
Karen <karenpenglabs@gmail.com>
2015-01-06 01:23:40 -0800

    too new

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
+// function* fibonacci() {
+//   let [prev, curr] = [0, 1];
+//   for (;;) {
+//     [prev, curr] = [curr, prev + curr];
+//     yield curr;
+//   }
+// }
+
 function* fibonacci() {
-  let [prev, curr] = [0, 1];
+  let prev = 0;
+  let curr = 1;
   for (;;) {
-    [prev, curr] = [curr, prev + curr];
+    let tmp = prev + curr;
+    prev = curr;
+    curr = tmp;
     yield curr;
   }
 }
@@ -10,5 +21,18 @@ for (n of fibonacci()) {
   // truncate the sequence at 1000
   if (n > 1000)
     break;
-  print(n);
+  console.log(n);
+}
+
+function fibonacii() {
+  var pre = 0;
+  var cur = 1;
+  console.log(pre);
+  console.log(cur);
+  while (true) {
+    var sum = pre + cur;
+    pre = cur;
+    cur = sum;
+    console.log(sum);
+  }
 }
