f4bd951da56f8d61262850bf609945ec239b5f63
Karen <karenpenglabs@gmail.com>
2015-01-13 16:23:19 -0800

    generate without generator

--- app.js
+++ app.js
@@ -5,34 +5,62 @@
 //     yield curr;
 //   }
 // }
+var later = require('later');
+// function* fibonacci() {
+//   let prev = 0;
+//   let curr = 1;
+//   for (;;) {
+//     let tmp = prev + curr;
+//     prev = curr;
+//     curr = tmp;
+//     yield curr;
+//   }
+// }
+var tweet = require('./tweet.js');
 
-function* fibonacci() {
-  let prev = 0;
-  let curr = 1;
-  for (;;) {
-    let tmp = prev + curr;
+function foo() {
+  var prev = 0;
+  var curr = 1;
+
+  return function () {
+    //console.log(pre);
+    var tmp = prev + curr;
     prev = curr;
     curr = tmp;
-    yield curr;
+    return curr;
   }
 }
 
-for (n of fibonacci()) {
-  // truncate the sequence at 1000
-  if (n > 1000)
-    break;
-  console.log(n);
-}
+//
+var test = foo();
+var sched = later.parse.recur().every(1).hour();
+later.setInterval(function () {
+  console.log(test());
+  tweet(test());
+}, sched);
 
-function fibonacii() {
-  var pre = 0;
-  var cur = 1;
-  console.log(pre);
-  console.log(cur);
-  while (true) {
-    var sum = pre + cur;
-    pre = cur;
-    cur = sum;
-    console.log(sum);
-  }
-}
+// for (n of fibonacci()) {
+//   // truncate the sequence at 1000
+//   if (n > 1000)
+//     break;
+//   console.log(n);
+// }
+
+// function fibonacii() {
+//   var pre = 0;
+//   var cur = 1;
+//   console.log(pre);
+//   console.log(cur);
+//   while (true) {
+//     var sum = pre + cur;
+//     pre = cur;
+//     cur = sum;
+//     console.log(sum);
+//   }
+// }
+
+later.date.UTC();
+var sched = later.parse.recur().on('03:50:00').time();
+later.setInterval(function () {
+  foo();
+}, sched);
