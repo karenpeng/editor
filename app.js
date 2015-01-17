8a831b49d78375e84145749e002c4dd61bfc7c84
Karen <karenpenglabs@gmail.com>
2015-01-17 01:52:11 -0500

    yeild number every one minute

--- app.js
+++ app.js
@@ -18,26 +18,40 @@ var later = require('later');
 // }
 var tweet = require('./tweet.js');
 
-function foo() {
-  var prev = 0;
-  var curr = 1;
+//function foo() {
+//  var prev = 0;
+//  var curr = 1;
 
-  return function () {
-    //console.log(pre);
-    var tmp = prev + curr;
-    var hold_prev = prev;
-    prev = curr;
-    curr = tmp;
-    return hold_prev;
-  }
+//  return function () {
+//    //console.log(pre);
+//    var tmp = prev + curr;
+//    var hold_prev = prev;
+//    prev = curr;
+//    curr = tmp;
+//    return hold_prev;
+//  }
+//}
+
+function* fibonacci2(){
+  let pre = 0, cur = 1, tmp;
+  yield pre;
+  yield cur;
+  while(true){
+   tmp = pre + cur;
+   pre = cur;
+   cur = tmp;
+   yield cur;
+ }
 }
 
-//why this line of code???
-var test = foo();
-var sched = later.parse.recur().every(1).hour();
+////why this line of code???
+//var test = foo();
+var test = fibonacci2();
+var sched = later.parse.recur().every(1).minute();
 later.setInterval(function () {
-  console.log(test());
-  tweet(test());
+  //console.log(test());
+  //tweet(test());
+  console.log(test.next());
 }, sched);
 
 // for (n of fibonacci()) {
